fc2ac9ffb4032dede80b93f33ebbb974c7f59ba4
Michael Hutchings <67608821+MHutchbot@users.noreply.github.com>
2020-12-14 02:05:02 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+// preemptive file
+// The Heroku Guide should help
+
+// has to be set up as an express server
+
+// dependencies
+const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+const path = require("path");
+
+// express data stuff (its parsing, just in case)
+// https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/routes
+const app = express();
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// linking above-said routes
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+module.exports = app;
+
+// SERVER PORT PLEASE REMEMBER
+const PORT = process.env.PORT || 8080;
+
+
+app.listen(PORT, function () {
+    console.log(`PORT ${PORT} is watching your every step`);
+});
